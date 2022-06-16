16e947ed74c555b776c70863ea5253f9378a6a67
James Boyle <james.patrick.boyle92@gmail.com>
2022-06-16 09:20:08 +0200

    Add techStack routes, model and controllers

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const projectsRouter = require("./routes/projects");
+const techRouter = require("./routes/techStack");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/projects", projectsRouter);
+app.use("/tech", techRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
