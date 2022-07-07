f5aa6413235c2914a9a98bfeb41b697b6fa90361
James Boyle <james.patrick.boyle92@gmail.com>
2022-07-07 13:52:27 +0200

    add transporterr to controller

--- controllers/email.js
+++ controllers/email.js
@@ -1,5 +1,23 @@
+const nodemailer = require("nodemailer");
+
 const sendEmail = async (req, res, next) => {
-  try {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: "james.patick.boyle92@gmail.com",
+      pass: "Jamesboyle5145-!",
+    },
+  });
+  transporter.verify((error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
+  /*  try {
     const {
       body: { email, subject, message },
     } = req;
@@ -18,7 +36,7 @@ const sendEmail = async (req, res, next) => {
       }
     });
     res.json({ message: "Email sent" });
-  } catch (error) {}
+  } catch (error) {} */
 };
 
 module.exports = {
